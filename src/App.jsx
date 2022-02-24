2df99dfe21f0eb3b6d47bc6d0878481f83138a34
radhad-ux <radhad83@gmail.com>
2022-02-24 23:45:41 +0100

    tech page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import AboutMe from "./pages/AboutMe";
 import Project from "./pages/Project";
+import Tech from "./pages/Tech";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <AboutMe />
       <Project />
+      <Tech />
     </div>
   );
 }
---
+++ src/pages/Tech.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import techdata from "../data/techdata.json";
+import TechItem from "../components/TechItem";
+
+const Tech = () => {
+  const TechList = techdata.map((data) => (
+    <TechItem key={data.id} data={data} />
+  ));
+
+  return (
+    <div className="container_main" id="project">
+      <h2 className="title">Technologies I Use</h2>
+      <div className="container_sub">
+        <div className="page_left">
+          <p>
+            Here is the technologies that I already know and also starting to
+            learn.
+          </p>
+        </div>
+        <div className="page_right">
+          <div className="tech_list">{TechList}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tech;

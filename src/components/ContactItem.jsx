9ef3813f78558819b9719265c68a68fb0cf4c536
radhad-ux <radhad83@gmail.com>
2022-03-02 20:17:44 +0100

    deleted unnecessary files

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-const ContactItem = ({ data }) => {
+export default function ContactItem({ data }) {
   const { title, link, image } = data;
   const imageURL = require(`../assets/images/contacticons/${image}`);
 
   return (
     <div className="contact_item">
-      <a href={link} target="_blank" rel="noreffer">
-        <img src={imageURL} className="contact_image" />
+      <a href={link} target="_blank" rel="noreferrer">
+        <img src={imageURL} className="contact_image" alt="contact icons" />
       </a>
       <span>{title}</span>
     </div>
   );
-};
-
-export default ContactItem;
+}
